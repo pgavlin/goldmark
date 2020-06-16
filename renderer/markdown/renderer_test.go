aaa9ff3e1d61723b56b2af6b809a44ae2ec843d0
Pat Gavlin <pgavlin@gmail.com>
2020-06-16 09:47:52 -0700

    Expose structural assertions from testutil.

--- renderer/markdown/renderer_test.go
+++ renderer/markdown/renderer_test.go
@@ -12,103 +12,10 @@ import (
 	"github.com/yuin/goldmark"
 	"github.com/yuin/goldmark/ast"
 	"github.com/yuin/goldmark/renderer"
+	"github.com/yuin/goldmark/testutil"
 	"github.com/yuin/goldmark/text"
 	"github.com/yuin/goldmark/util"
 )
-
-func assertEqualBytes(t *testing.T, a, b []byte) bool {
-	if len(a) == 0 {
-		a = nil
-	}
-	if len(b) == 0 {
-		b = nil
-	}
-	return assert.Equal(t, a, b)
-}
-
-func assertSameStructure(t *testing.T, sa, sb []byte, a, b ast.Node) bool {
-	if !assert.Equal(t, a.Kind().String(), b.Kind().String()) {
-		return false
-	}
-
-	var ok bool
-	switch a.Kind() {
-	case ast.KindAutoLink:
-		na, nb := a.(*ast.AutoLink), b.(*ast.AutoLink)
-		ok = assert.Equal(t, na.AutoLinkType, nb.AutoLinkType) &&
-			assert.Equal(t, na.Protocol, nb.Protocol) &&
-			assert.Equal(t, na.Label(sa), nb.Label(sb))
-	case ast.KindEmphasis:
-		na, nb := a.(*ast.Emphasis), b.(*ast.Emphasis)
-		ok = assert.Equal(t, na.Level, nb.Level)
-	case ast.KindFencedCodeBlock:
-		na, nb := a.(*ast.FencedCodeBlock), b.(*ast.FencedCodeBlock)
-		ok = assert.Equal(t, na.Language(sa), nb.Language(sb))
-	case ast.KindHTMLBlock:
-		assert.Equal(t, a.Text(sa), b.Text(sb))
-	case ast.KindHeading:
-		na, nb := a.(*ast.Heading), b.(*ast.Heading)
-		ok = assert.Equal(t, na.Level, nb.Level)
-	case ast.KindImage:
-		na, nb := a.(*ast.Image), b.(*ast.Image)
-		ok = assert.Equal(t, na.Destination, nb.Destination) &&
-			assert.Equal(t, na.Title, nb.Title)
-	case ast.KindLink:
-		na, nb := a.(*ast.Link), b.(*ast.Link)
-		ok = assert.Equal(t, na.ReferenceType, nb.ReferenceType) &&
-			assertEqualBytes(t, na.Label, nb.Label) &&
-			assertEqualBytes(t, na.Destination, nb.Destination) &&
-			assertEqualBytes(t, na.Title, nb.Title)
-	case ast.KindLinkReferenceDefinition:
-		na, nb := a.(*ast.LinkReferenceDefinition), b.(*ast.LinkReferenceDefinition)
-		ok = assertEqualBytes(t, na.Label, nb.Label) &&
-			assertEqualBytes(t, na.Destination, nb.Destination) &&
-			assertEqualBytes(t, na.Title, nb.Title)
-	case ast.KindList:
-		na, nb := a.(*ast.List), b.(*ast.List)
-		ok = assert.Equal(t, na.Marker, nb.Marker) &&
-			assert.Equal(t, na.Start, nb.Start) &&
-			assert.Equal(t, na.IsTight, nb.IsTight)
-	case ast.KindRawHTML:
-		na, nb := a.(*ast.RawHTML), b.(*ast.RawHTML)
-		ok = assert.Equal(t, na.Text(sa), nb.Text(sb))
-	case ast.KindString:
-		na, nb := a.(*ast.String), b.(*ast.String)
-		ok = assert.Equal(t, na.Value, nb.Value) &&
-			assert.Equal(t, na.IsRaw(), nb.IsRaw())
-	case ast.KindText:
-		na, nb := a.(*ast.Text), b.(*ast.Text)
-		ok = assert.Equal(t, na.Text(sa), nb.Text(sb)) &&
-			assert.Equal(t, na.SoftLineBreak(), nb.SoftLineBreak()) &&
-			assert.Equal(t, na.HardLineBreak(), nb.HardLineBreak()) &&
-			assert.Equal(t, na.IsRaw(), nb.IsRaw())
-	case ast.KindWhitespace:
-		na, nb := a.(*ast.Whitespace), b.(*ast.Whitespace)
-		ok = assertEqualBytes(t, na.Segment.Value(sa), nb.Segment.Value(sb))
-	case ast.KindBlockquote, ast.KindCodeBlock, ast.KindCodeSpan, ast.KindDocument, ast.KindListItem, ast.KindParagraph,
-		ast.KindTextBlock, ast.KindThematicBreak:
-		ok = true
-
-		// Nothing extra to check
-	default:
-		t.Logf("unexpected node kind %v", a.Kind())
-	}
-	if !ok {
-		return false
-	}
-
-	if !assert.Equal(t, a.ChildCount(), b.ChildCount()) {
-		return false
-	}
-
-	for c, d := a.FirstChild(), b.FirstChild(); c != nil; c, d = c.NextSibling(), d.NextSibling() {
-		if !assertSameStructure(t, sa, sb, c, d) {
-			return false
-		}
-	}
-
-	return true
-}
 
 type commonmarkSpecTestCase struct {
 	Markdown string `json:"markdown"`
@@ -160,7 +67,7 @@ func TestSpec(t *testing.T) {
 			sourceActual := buf.Bytes()
 			actual := parser.Parse(text.NewReader(sourceActual))
 
-			if !assertSameStructure(t, sourceExpected, sourceActual, expected, actual) {
+			if !testutil.AssertSameStructure(t, sourceExpected, sourceActual, expected, actual, testutil.DefaultNodeAssertions()) {
 				t.Logf("case %d:", c.Example)
 
 				t.Logf("expected: %q", string(sourceExpected))
--- testutil/testutil.go
+++ testutil/testutil.go
@@ -8,8 +8,11 @@ import (
 	"runtime/debug"
 	"strconv"
 	"strings"
+	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark/ast"
 	"github.com/yuin/goldmark/util"
 )
 
@@ -152,4 +155,146 @@ Actual
 		panic(err)
 	}
 	ok = bytes.Equal(bytes.TrimSpace(out.Bytes()), bytes.TrimSpace([]byte(testCase.Expected)))
+}
+
+// AssertNodeFunc is used by AssertSameStructure to assert that two nodes are semantically identical.
+type AssertNodeFunc func(t *testing.T, sourceA, sourceB []byte, a, b ast.Node) bool
+
+func assertNodeNoop(t *testing.T, sa, sb []byte, a, b ast.Node) bool {
+	return true
+}
+
+// NodeAssertions maps from node kinds to AssertNodeFunc.
+type NodeAssertions map[ast.NodeKind]AssertNodeFunc
+
+// Union returns a new set of node assertions that is the union of the two input sets.
+func (na NodeAssertions) Union(other NodeAssertions) NodeAssertions {
+	m := NodeAssertions{}
+	for k, f := range na {
+		m[k] = f
+	}
+	for k, f := range other {
+		m[k] = f
+	}
+	return m
+}
+
+// DefaultNodeAssertions returns the default set of node assertions.
+func DefaultNodeAssertions() NodeAssertions {
+	return NodeAssertions{
+		ast.KindAutoLink: func(t *testing.T, sa, sb []byte, a, b ast.Node) bool {
+			na, nb := a.(*ast.AutoLink), b.(*ast.AutoLink)
+			return assert.Equal(t, na.AutoLinkType, nb.AutoLinkType) &&
+				assert.Equal(t, na.Protocol, nb.Protocol) &&
+				assert.Equal(t, na.Label(sa), nb.Label(sb))
+		},
+		ast.KindEmphasis: func(t *testing.T, sa, sb []byte, a, b ast.Node) bool {
+			na, nb := a.(*ast.Emphasis), b.(*ast.Emphasis)
+			return assert.Equal(t, na.Level, nb.Level)
+		},
+		ast.KindFencedCodeBlock: func(t *testing.T, sa, sb []byte, a, b ast.Node) bool {
+			na, nb := a.(*ast.FencedCodeBlock), b.(*ast.FencedCodeBlock)
+			return assert.Equal(t, na.Language(sa), nb.Language(sb))
+		},
+		ast.KindHTMLBlock: func(t *testing.T, sa, sb []byte, a, b ast.Node) bool {
+			return assert.Equal(t, a.Text(sa), b.Text(sb))
+		},
+		ast.KindHeading: func(t *testing.T, sa, sb []byte, a, b ast.Node) bool {
+			na, nb := a.(*ast.Heading), b.(*ast.Heading)
+			return assert.Equal(t, na.Level, nb.Level)
+		},
+		ast.KindImage: func(t *testing.T, sa, sb []byte, a, b ast.Node) bool {
+			na, nb := a.(*ast.Image), b.(*ast.Image)
+			return assert.Equal(t, na.Destination, nb.Destination) &&
+				assert.Equal(t, na.Title, nb.Title)
+		},
+		ast.KindLink: func(t *testing.T, sa, sb []byte, a, b ast.Node) bool {
+			na, nb := a.(*ast.Link), b.(*ast.Link)
+			return assert.Equal(t, na.ReferenceType, nb.ReferenceType) &&
+				AssertEqualBytes(t, na.Label, nb.Label) &&
+				AssertEqualBytes(t, na.Destination, nb.Destination) &&
+				AssertEqualBytes(t, na.Title, nb.Title)
+		},
+		ast.KindLinkReferenceDefinition: func(t *testing.T, sa, sb []byte, a, b ast.Node) bool {
+			na, nb := a.(*ast.LinkReferenceDefinition), b.(*ast.LinkReferenceDefinition)
+			return AssertEqualBytes(t, na.Label, nb.Label) &&
+				AssertEqualBytes(t, na.Destination, nb.Destination) &&
+				AssertEqualBytes(t, na.Title, nb.Title)
+		},
+		ast.KindList: func(t *testing.T, sa, sb []byte, a, b ast.Node) bool {
+			na, nb := a.(*ast.List), b.(*ast.List)
+			return assert.Equal(t, na.Marker, nb.Marker) &&
+				assert.Equal(t, na.Start, nb.Start) &&
+				assert.Equal(t, na.IsTight, nb.IsTight)
+		},
+		ast.KindRawHTML: func(t *testing.T, sa, sb []byte, a, b ast.Node) bool {
+			na, nb := a.(*ast.RawHTML), b.(*ast.RawHTML)
+			return assert.Equal(t, na.Text(sa), nb.Text(sb))
+		},
+		ast.KindString: func(t *testing.T, sa, sb []byte, a, b ast.Node) bool {
+			na, nb := a.(*ast.String), b.(*ast.String)
+			return assert.Equal(t, na.Value, nb.Value) &&
+				assert.Equal(t, na.IsRaw(), nb.IsRaw())
+		},
+		ast.KindText: func(t *testing.T, sa, sb []byte, a, b ast.Node) bool {
+			na, nb := a.(*ast.Text), b.(*ast.Text)
+			return assert.Equal(t, na.Text(sa), nb.Text(sb)) &&
+				assert.Equal(t, na.SoftLineBreak(), nb.SoftLineBreak()) &&
+				assert.Equal(t, na.HardLineBreak(), nb.HardLineBreak()) &&
+				assert.Equal(t, na.IsRaw(), nb.IsRaw())
+		},
+		ast.KindWhitespace: func(t *testing.T, sa, sb []byte, a, b ast.Node) bool {
+			na, nb := a.(*ast.Whitespace), b.(*ast.Whitespace)
+			return AssertEqualBytes(t, na.Segment.Value(sa), nb.Segment.Value(sb))
+		},
+		ast.KindBlockquote:    assertNodeNoop,
+		ast.KindCodeBlock:     assertNodeNoop,
+		ast.KindCodeSpan:      assertNodeNoop,
+		ast.KindDocument:      assertNodeNoop,
+		ast.KindListItem:      assertNodeNoop,
+		ast.KindParagraph:     assertNodeNoop,
+		ast.KindTextBlock:     assertNodeNoop,
+		ast.KindThematicBreak: assertNodeNoop,
+	}
+}
+
+// AssertEqualBytes asserts that the two input byte slices have the same length and contents.
+func AssertEqualBytes(t *testing.T, a, b []byte) bool {
+	if len(a) == 0 {
+		a = nil
+	}
+	if len(b) == 0 {
+		b = nil
+	}
+	return assert.Equal(t, a, b)
+}
+
+func assertSameStructure(t *testing.T, sa, sb []byte, a, b ast.Node, assertions NodeAssertions) bool {
+	if !assert.Equal(t, a.Kind().String(), b.Kind().String()) {
+		return false
+	}
+
+	assertFunc, ok := assertions[a.Kind()]
+	if !ok {
+		t.Logf("unexpected node kind %v", a.Kind())
+	} else if !assertFunc(t, sa, sb, a, b) {
+		return false
+	}
+
+	if !assert.Equal(t, a.ChildCount(), b.ChildCount()) {
+		return false
+	}
+
+	for c, d := a.FirstChild(), b.FirstChild(); c != nil; c, d = c.NextSibling(), d.NextSibling() {
+		if !assertSameStructure(t, sa, sb, c, d, assertions) {
+			return false
+		}
+	}
+
+	return true
+}
+
+// AssertSameStructure walks the ASTs rooted at a and b
+func AssertSameStructure(t *testing.T, sourceA, sourceB []byte, a, b ast.Node, assertions NodeAssertions) bool {
+	return assertSameStructure(t, sourceA, sourceB, a, b, assertions)
 }
